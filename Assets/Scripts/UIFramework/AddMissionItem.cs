966b6d4d9eb2b5a85290eb92952b60c35fe303f2
AMIYA-ZY\沢鱼 <409657111@qq.com>
2023-03-06 23:50:57 +0800

    [dev] 移动添加Item按钮的脚本的文件位置

--- Assets/Scripts/UIFramework/AddMissionItem.cs
+++ Assets/Scripts/UIFramework/AddMissionItem.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AddMissionItem : MonoBehaviour
 {
+    private long maxMissionItemID;
     public GameObject MissionItemPrefab;
     public Transform CatalogToAdd;
 
     public void CreateMissionItem()
     {
         GameObject newGameObject = Instantiate(MissionItemPrefab, CatalogToAdd);
+        
     }
 }
